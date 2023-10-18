c14c60c46df5cf0cc4d3760cb4ad1aba0cff275f
Filipe Pereira <139193972+filipepereira03@users.noreply.github.com>
2023-10-18 00:15:24 -0300

    Update ContaBancária.java

--- POO/ContaBancária.java
+++ POO/ContaBancária.java
@@ -1,46 +1,48 @@
-public class ContaBancária {
+class ContaBancaria {
     private double saldo;
-    private double rendimentoMensal;
+    private double rendimentoMensalPercentual;
 
-    public ContaBancária(double saldo, double rendimentoMensal) {
-        this.saldo = saldo;
-        this.rendimentoMensal = rendimentoMensal;
+    public ContaBancaria(double saldoInicial, double rendimentoMensalPercentual) {
+        this.saldo = saldoInicial;
+        this.rendimentoMensalPercentual = rendimentoMensalPercentual;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para o saque.");
+        }
+    }
+
+    public void calcularRendimentoMensal() {
+        double rendimento = saldo * (rendimentoMensalPercentual / 100);
+        saldo += rendimento;
+        System.out.println("Rendimento mensal de R$" + rendimento);
+    }
+
+    public double preverRendimentoMensalComAporte(int meses, double aporteMensal) {
+        for (int i = 0; i < meses; i++) {
+            calcularRendimentoMensal();
+            saldo += aporteMensal;
+        }
+        return saldo;
+    }
+
+    public double preverRendimentoMensal(int meses) {
+        for (int i = 0; i < meses; i++) {
+            calcularRendimentoMensal();
+        }
+        return saldo;
     }
 
     public double getSaldo() {
         return saldo;
-    }
-
-    public double getRendimentoMensal() {
-        return rendimentoMensal;
-    }
-
-    public void setSaldo(double novoSaldo) {
-        this.saldo = novoSaldo;
-    }
-
-    public void setRendimentoMensal(double novoRendimentoMensal) {
-        this.rendimentoMensal = novoRendimentoMensal;
-    }
-
-    public void deposito(double deposito) {
-        saldo += deposito;
-    }
-
-    public boolean sacar(double saque) {
-        if (saldo >= saque) {
-            saldo -= saque;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public double calcularRendimento() {
-        return saldo * rendimentoMensal;
-    }
-
-    public double preverRendimento(int n) {
-        return saldo * n * rendimentoMensal;
     }
 }
